6478ee749fdea20463e077df097390ebb4458fad
vaedu <vaedu070418@gmail.com>
2024-12-09 21:02:36 +0800

    更新：添加将已转换的数字转换回10进制的功能，修改了回退按钮的功能

--- entry/.preview/default/cache/default/default@PreviewArkTS/esmodule/debug/entry/src/main/ets/pages/Index.ts
+++ entry/.preview/default/cache/default/default@PreviewArkTS/esmodule/debug/entry/src/main/ets/pages/Index.ts
@@ -3,6 +3,7 @@ if (!("finalizeConstruction" in ViewPU.prototype)) {
 }
 interface Index_Params {
     num1?: string;
+    convertedNum?: string;
     numberArr?: string[];
 }
 function __Button__ButtonStyles(): void {
@@ -19,12 +20,14 @@ class Index extends ViewPU {
         }
         this.__num1 = new ObservedPropertySimplePU('' // 用户输入的十进制数字
         , this, "num1");
+        this.__convertedNum = new ObservedPropertySimplePU('' // 存储转换后的数字
+        , this, "convertedNum");
         this.__numberArr = new ObservedPropertyObjectPU([
             'C', '2进制', '4进制', '⌫',
             '7', '8', '9', '8进制',
             '4', '5', '6', '16进制',
             '1', '2', '3', '32进制',
-            '0'
+            '10进制', '0'
         ]
         // 进制转换函数
         , this, "numberArr");
@@ -35,6 +38,9 @@ class Index extends ViewPU {
         if (params.num1 !== undefined) {
             this.num1 = params.num1;
         }
+        if (params.convertedNum !== undefined) {
+            this.convertedNum = params.convertedNum;
+        }
         if (params.numberArr !== undefined) {
             this.numberArr = params.numberArr;
         }
@@ -43,10 +49,12 @@ class Index extends ViewPU {
     }
     purgeVariableDependenciesOnElmtId(rmElmtId) {
         this.__num1.purgeDependencyOnElmtId(rmElmtId);
+        this.__convertedNum.purgeDependencyOnElmtId(rmElmtId);
         this.__numberArr.purgeDependencyOnElmtId(rmElmtId);
     }
     aboutToBeDeleted() {
         this.__num1.aboutToBeDeleted();
+        this.__convertedNum.aboutToBeDeleted();
         this.__numberArr.aboutToBeDeleted();
         SubscriberManager.Get().delete(this.id__());
         this.aboutToBeDeletedInternal();
@@ -57,6 +65,13 @@ class Index extends ViewPU {
     }
     set num1(newValue: string) {
         this.__num1.set(newValue);
+    }
+    private __convertedNum: ObservedPropertySimplePU<string>; // 存储转换后的数字
+    get convertedNum() {
+        return this.__convertedNum.get();
+    }
+    set convertedNum(newValue: string) {
+        this.__convertedNum.set(newValue);
     }
     private __numberArr: ObservedPropertyObjectPU<string[]>;
     get numberArr() {
@@ -80,27 +95,24 @@ class Index extends ViewPU {
     initialRender() {
         this.observeComponentCreation2((elmtId, isInitialRender) => {
             Column.create();
-            Column.debugLine("entry/src/main/ets/pages/Index.ets(33:7)", "entry");
+            Column.debugLine("entry/src/main/ets/pages/Index.ets(33:5)", "entry");
             Column.width('100%');
             Column.height(1000);
             Column.backgroundColor('#000');
             Column.justifyContent(FlexAlign.SpaceBetween);
         }, Column);
         this.observeComponentCreation2((elmtId, isInitialRender) => {
-            Column.create();
-            Column.debugLine("entry/src/main/ets/pages/Index.ets(35:9)", "entry");
-            Column.width('100%');
-            Column.height(120);
-        }, Column);
-        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            // 显示输入或转换后的数字
             Row.create();
-            Row.debugLine("entry/src/main/ets/pages/Index.ets(36:11)", "entry");
+            Row.debugLine("entry/src/main/ets/pages/Index.ets(36:7)", "entry");
+            // 显示输入或转换后的数字
             Row.width('100%');
-            Row.height('100%');
+            // 显示输入或转换后的数字
+            Row.height(120);
         }, Row);
         this.observeComponentCreation2((elmtId, isInitialRender) => {
-            Text.create(this.num1);
-            Text.debugLine("entry/src/main/ets/pages/Index.ets(37:13)", "entry");
+            Text.create(this.convertedNum || this.num1 || '0');
+            Text.debugLine("entry/src/main/ets/pages/Index.ets(37:9)", "entry");
             Text.width('100%');
             Text.height(200);
             Text.backgroundColor("#010101");
@@ -110,15 +122,16 @@ class Index extends ViewPU {
             Text.padding(10);
         }, Text);
         Text.pop();
+        // 显示输入或转换后的数字
         Row.pop();
-        Column.pop();
         this.observeComponentCreation2((elmtId, isInitialRender) => {
             Column.create();
-            Column.debugLine("entry/src/main/ets/pages/Index.ets(52:9)", "entry");
+            Column.debugLine("entry/src/main/ets/pages/Index.ets(49:7)", "entry");
             Column.width('100%');
             Column.height(880);
         }, Column);
         this.observeComponentCreation2((elmtId, isInitialRender) => {
+            // 创建按钮布局
             ForEach.create();
             const forEachItemGenFunction = (_item, index: number) => {
                 const item = _item;
@@ -128,13 +141,11 @@ class Index extends ViewPU {
                         this.ifElseBranchUpdateFunction(0, () => {
                             this.observeComponentCreation2((elmtId, isInitialRender) => {
                                 Row.create();
-                                Row.debugLine("entry/src/main/ets/pages/Index.ets(55:15)", "entry");
+                                Row.debugLine("entry/src/main/ets/pages/Index.ets(53:13)", "entry");
                                 Row.width('100%');
                                 Row.height(120);
                                 Row.justifyContent(FlexAlign.SpaceEvenly);
-                                Row.margin({
-                                    bottom: 10
-                                });
+                                Row.margin({ bottom: 10 });
                             }, Row);
                             this.observeComponentCreation2((elmtId, isInitialRender) => {
                                 ForEach.create();
@@ -142,7 +153,7 @@ class Index extends ViewPU {
                                     const buttonItem = _item;
                                     this.observeComponentCreation2((elmtId, isInitialRender) => {
                                         Button.createWithLabel(buttonItem);
-                                        Button.debugLine("entry/src/main/ets/pages/Index.ets(57:19)", "entry");
+                                        Button.debugLine("entry/src/main/ets/pages/Index.ets(55:17)", "entry");
                                         __Button__ButtonStyles();
                                         ViewStackProcessor.visualState("normal");
                                         Button.backgroundColor('#000');
@@ -150,29 +161,57 @@ class Index extends ViewPU {
                                         Button.backgroundColor('#70eeeeee');
                                         ViewStackProcessor.visualState();
                                         Button.onClick(() => {
+                                            // 判断按钮功能
                                             if (buttonItem === 'C') {
+                                                // 清空输入
                                                 this.num1 = '';
+                                                this.convertedNum = '';
                                             }
                                             else if (buttonItem === '⌫') {
-                                                this.num1 = this.num1.slice(0, -1);
+                                                // 回退按钮
+                                                if (this.convertedNum) {
+                                                    this.convertedNum = '';
+                                                    this.num1 = '';
+                                                }
+                                                else {
+                                                    this.num1 = this.num1.slice(0, -1);
+                                                }
                                             }
                                             else if (buttonItem === '2进制') {
-                                                this.num1 = this.convertToBase(this.num1, 2);
+                                                // 转换为2进制
+                                                this.convertedNum = this.convertToBase(this.num1, 2);
                                             }
                                             else if (buttonItem === '4进制') {
-                                                this.num1 = this.convertToBase(this.num1, 4);
+                                                // 转换为4进制
+                                                this.convertedNum = this.convertToBase(this.num1, 4);
                                             }
                                             else if (buttonItem === '8进制') {
-                                                this.num1 = this.convertToBase(this.num1, 8);
+                                                // 转换为8进制
+                                                this.convertedNum = this.convertToBase(this.num1, 8);
                                             }
                                             else if (buttonItem === '16进制') {
-                                                this.num1 = this.convertToBase(this.num1, 16);
+                                                // 转换为16进制
+                                                this.convertedNum = this.convertToBase(this.num1, 16);
                                             }
                                             else if (buttonItem === '32进制') {
-                                                this.num1 = this.convertToBase(this.num1, 32);
+                                                // 转换为32进制
+                                                this.convertedNum = this.convertToBase(this.num1, 32);
+                                            }
+                                            else if (buttonItem === '10进制') {
+                                                // 转换为十进制
+                                                this.convertedNum = this.convertToBase(this.num1, 10);
                                             }
                                             else {
-                                                this.num1 += buttonItem;
+                                                // 输入数字或小数点
+                                                if (this.convertedNum) {
+                                                    // 已转换为其他进制，清空转换结果，开始新的输入
+                                                    this.convertedNum = '';
+                                                    this.num1 = buttonItem;
+                                                }
+                                                else {
+                                                    // 正常输入数字
+                                                    this.num1 += buttonItem;
+                                                }
                                             }
                                         });
                                     }, Button);
@@ -193,6 +232,7 @@ class Index extends ViewPU {
             };
             this.forEachUpdateFunction(elmtId, this.numberArr, forEachItemGenFunction, undefined, true, false);
         }, ForEach);
+        // 创建按钮布局
         ForEach.pop();
         Column.pop();
         Column.pop();
--- entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/pages/Index.ts
+++ entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/pages/Index.ts
@@ -3,13 +3,13 @@ if (!("finalizeConstruction" in ViewPU.prototype)) {
 }
 interface Index_Params {
     num1?: string;
+    convertedNum?: string;
     numberArr?: string[];
 }
 function __Button__ButtonStyles(): void {
     Button.width('22%');
     Button.height(120);
     Button.type(ButtonType.Normal);
-    Button.fontColor('#fff');
 }
 class Index extends ViewPU {
     constructor(parent, params, __localStorage, elmtId = -1, paramsLambda = undefined, extraInfo) {
@@ -17,14 +17,16 @@ class Index extends ViewPU {
         if (typeof paramsLambda === "function") {
             this.paramsGenerator_ = paramsLambda;
         }
-        this.__num1 = new ObservedPropertySimplePU('' // 用户输入的十进制数字
+        this.__num1 = new ObservedPropertySimplePU('' // 存储用户输入的原始数字
         , this, "num1");
+        this.__convertedNum = new ObservedPropertySimplePU('' // 存储转换后的数字
+        , this, "convertedNum");
         this.__numberArr = new ObservedPropertyObjectPU([
             'C', '2进制', '4进制', '⌫',
             '7', '8', '9', '8进制',
             '4', '5', '6', '16进制',
             '1', '2', '3', '32进制',
-            '0'
+            '0', '10进制'
         ]
         // 进制转换函数
         , this, "numberArr");
@@ -35,6 +37,9 @@ class Index extends ViewPU {
         if (params.num1 !== undefined) {
             this.num1 = params.num1;
         }
+        if (params.convertedNum !== undefined) {
+            this.convertedNum = params.convertedNum;
+        }
         if (params.numberArr !== undefined) {
             this.numberArr = params.numberArr;
         }
@@ -43,20 +48,29 @@ class Index extends ViewPU {
     }
     purgeVariableDependenciesOnElmtId(rmElmtId) {
         this.__num1.purgeDependencyOnElmtId(rmElmtId);
+        this.__convertedNum.purgeDependencyOnElmtId(rmElmtId);
         this.__numberArr.purgeDependencyOnElmtId(rmElmtId);
     }
     aboutToBeDeleted() {
         this.__num1.aboutToBeDeleted();
+        this.__convertedNum.aboutToBeDeleted();
         this.__numberArr.aboutToBeDeleted();
         SubscriberManager.Get().delete(this.id__());
         this.aboutToBeDeletedInternal();
     }
-    private __num1: ObservedPropertySimplePU<string>; // 用户输入的十进制数字
+    private __num1: ObservedPropertySimplePU<string>; // 存储用户输入的原始数字
     get num1() {
         return this.__num1.get();
     }
     set num1(newValue: string) {
         this.__num1.set(newValue);
+    }
+    private __convertedNum: ObservedPropertySimplePU<string>; // 存储转换后的数字
+    get convertedNum() {
+        return this.__convertedNum.get();
+    }
+    set convertedNum(newValue: string) {
+        this.__convertedNum.set(newValue);
     }
     private __numberArr: ObservedPropertyObjectPU<string[]>;
     get numberArr() {
@@ -77,28 +91,40 @@ class Index extends ViewPU {
             return "错误";
         }
     }
+    // 将转换后的数字转换回十进制
+    convertToDecimal(number: string): string {
+        try {
+            const decimalValue = parseInt(number, 10); // 将当前数字按十进制转换
+            return decimalValue.toString(10); // 转换回十进制
+        }
+        catch {
+            return "错误";
+        }
+    }
     initialRender() {
         this.observeComponentCreation2((elmtId, isInitialRender) => {
-            Column.create();
-            Column.width('100%');
-            Column.height(1000);
-            Column.backgroundColor('#000');
-            Column.justifyContent(FlexAlign.SpaceBetween);
-        }, Column);
+            Navigation.create(new NavPathStack(), { moduleName: "entry", pagePath: "entry/src/main/ets/pages/Index", isUserCreateStack: false });
+            Navigation.hideToolBar(true);
+            Navigation.hideTitleBar(true);
+            Navigation.width('100%');
+            Navigation.height('100%');
+            Navigation.backgroundColor('#000');
+        }, Navigation);
         this.observeComponentCreation2((elmtId, isInitialRender) => {
             Column.create();
             Column.width('100%');
-            Column.height(120);
+            Column.height('150%');
+            Column.justifyContent(FlexAlign.SpaceEvenly);
         }, Column);
         this.observeComponentCreation2((elmtId, isInitialRender) => {
             Row.create();
             Row.width('100%');
-            Row.height('100%');
+            Row.height(120);
         }, Row);
         this.observeComponentCreation2((elmtId, isInitialRender) => {
-            Text.create(this.num1);
+            Text.create(this.num1 || this.convertedNum);
             Text.width('100%');
-            Text.height(200);
+            Text.height(100);
             Text.backgroundColor("#010101");
             Text.fontColor('#fff');
             Text.fontSize(30);
@@ -107,11 +133,10 @@ class Index extends ViewPU {
         }, Text);
         Text.pop();
         Row.pop();
-        Column.pop();
         this.observeComponentCreation2((elmtId, isInitialRender) => {
             Column.create();
             Column.width('100%');
-            Column.height(880);
+            Column.height('100%');
         }, Column);
         this.observeComponentCreation2((elmtId, isInitialRender) => {
             ForEach.create();
@@ -124,7 +149,7 @@ class Index extends ViewPU {
                             this.observeComponentCreation2((elmtId, isInitialRender) => {
                                 Row.create();
                                 Row.width('100%');
-                                Row.height(120);
+                                Row.height(100);
                                 Row.justifyContent(FlexAlign.SpaceEvenly);
                                 Row.margin({
                                     bottom: 10
@@ -138,34 +163,60 @@ class Index extends ViewPU {
                                         Button.createWithLabel(buttonItem);
                                         __Button__ButtonStyles();
                                         ViewStackProcessor.visualState("normal");
+                                        Button.fontColor('#fff');
                                         Button.backgroundColor('#000');
                                         ViewStackProcessor.visualState("pressed");
-                                        Button.backgroundColor('#70eeeeee');
+                                        Button.fontColor('#000');
+                                        Button.backgroundColor('#fff');
                                         ViewStackProcessor.visualState();
                                         Button.onClick(() => {
                                             if (buttonItem === 'C') {
+                                                // 清除按钮
                                                 this.num1 = '';
+                                                this.convertedNum = '';
                                             }
                                             else if (buttonItem === '⌫') {
-                                                this.num1 = this.num1.slice(0, -1);
+                                                // 回退按钮
+                                                if (this.convertedNum) {
+                                                    this.convertedNum = this.convertToDecimal(this.convertedNum);
+                                                }
+                                                else {
+                                                    this.num1 = this.num1.slice(0, -1);
+                                                }
                                             }
                                             else if (buttonItem === '2进制') {
-                                                this.num1 = this.convertToBase(this.num1, 2);
+                                                // 转换为2进制
+                                                this.convertedNum = this.convertToBase(this.num1, 2);
                                             }
                                             else if (buttonItem === '4进制') {
-                                                this.num1 = this.convertToBase(this.num1, 4);
+                                                // 转换为4进制
+                                                this.convertedNum = this.convertToBase(this.num1, 4);
                                             }
                                             else if (buttonItem === '8进制') {
-                                                this.num1 = this.convertToBase(this.num1, 8);
+                                                // 转换为8进制
+                                                this.convertedNum = this.convertToBase(this.num1, 8);
                                             }
                                             else if (buttonItem === '16进制') {
-                                                this.num1 = this.convertToBase(this.num1, 16);
+                                                // 转换为16进制
+                                                this.convertedNum = this.convertToBase(this.num1, 16);
                                             }
                                             else if (buttonItem === '32进制') {
-                                                this.num1 = this.convertToBase(this.num1, 32);
+                                                // 转换为32进制
+                                                this.convertedNum = this.convertToBase(this.num1, 32);
+                                            }
+                                            else if (buttonItem === '10进制') {
+                                                // 将当前转换数字转换回十进制
+                                                this.convertedNum = this.convertToDecimal(this.convertedNum || this.num1);
                                             }
                                             else {
-                                                this.num1 += buttonItem;
+                                                // 数字或小数点
+                                                if (this.convertedNum) {
+                                                    this.num1 = buttonItem; // 如果已经转换过进制，继续输入新的数字
+                                                    this.convertedNum = ''; // 清空之前的转换结果
+                                                }
+                                                else {
+                                                    this.num1 += buttonItem; // 否则直接输入数字
+                                                }
                                             }
                                         });
                                     }, Button);
@@ -189,6 +240,7 @@ class Index extends ViewPU {
         ForEach.pop();
         Column.pop();
         Column.pop();
+        Navigation.pop();
     }
     rerender() {
         this.updateDirtyElements();
